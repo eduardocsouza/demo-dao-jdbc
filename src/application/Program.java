3f243a55a0c7f52f8c1abd43f204410c4a9def3d
eduardocsouza <dudusoja@gmail.com>
2019-08-22 15:26:08 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Department obj = new Department(1, "books");
+		System.out.println(obj);
 	}
 
 }
